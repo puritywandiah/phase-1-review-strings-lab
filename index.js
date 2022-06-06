f857b7a3ac3af50ff81552284358a8d863d8b359
+ <+>
2022-06-06 18:12:27 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = 'Purity Wandia';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}`;
+
+const shortGreeting = "Welcome, P!"; 
+
+
